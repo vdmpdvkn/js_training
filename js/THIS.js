ac89dc02c7e8cecba647345ad50d92d4a7b25f43
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-02-09 21:20:59 +0300

    newTask

---
+++ js/THIS.js
@@ -0,0 +1,28 @@
+console.log("hello");
+/**
+*? Напиши функцію конструктор User для 
+*? створення користувача з такими властивостями:
+*? a. userName - ім'я, рядок
+*? b. age - вік, число
+*? c. numbersOfPost - кількість постів, число
+*? d. клас очікує 1 параметр - об'єкт налаштувань з однойменними властивостями
+
+*? Додай метод getInfo(), який повертає рядок:
+*? `Користувачеві ${ім'я} ${вік} років і в нього ${кількістьПублікацій} публікацій.`
+
+ */
+
+// function User(options) {
+//   const { name, age, postCount } = options;
+//   this.name = name;
+//   this.age = age;
+//   this.postCount = postCount;
+// }
+// User.prototype.getInfo = function () {
+//   return `Користувачеві ${this.name} ${this.age} років і в нього ${this.postCount} публікацій.`;
+// };
+
+// const lena = new User({ name: "Lena", age: 25, postCount: 11 });
+
+// console.log(lena);
+// console.log(lena.getInfo());
