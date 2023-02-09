df592a230e9912a9f0ed70234ac535de0945362a
Siracenco Serghei <siracencoserghei@gmail.com>
2023-02-09 20:35:56 +0100

    add exemple

--- js/THIS.js
+++ js/THIS.js
@@ -27,7 +27,6 @@ console.log("hello");
 // console.log(lena);
 // console.log(lena.getInfo());
 
-
 /**
  *? Напиши функцію конструктор Storage який створює об'єкти
  *? Для управління складом товарів.
@@ -60,4 +59,50 @@ console.log("hello");
 //  console.log(storage.getItems());
 
 //  storage.removeItem("add");
+
 //  console.log(storage.getItems());
+
+// ---------------------------
+
+/** *? Напиши клас Notes який управляє колекцією нотаток у
+ *
+ * *? властивості items. *? Нотатка - це об'єкт із властивостями text і priority.
+ * *? Додай класу статичний метод Priopity,
+ * *? який буде повертати об'єкт із пріоритетами.
+ * *? Додай методи addNote(note), removeNote(text)
+ * *? updatePriority(text, newPriority)
+ * */ // note1.addNote({ text: "Note1", priority: Notes.Priority().LOW });
+// note1.addNote({ text: "Note2", priority: Notes.Priority().LOW });
+// note1.updatePriority({ text: "Note2", newPriority: Notes.Priority().HIGHT });
+// console.table(note1.items);
+
+class Notes {
+  static Priority() {
+    return {
+      LOW: "low",
+      HIGHT: "hight",
+    };
+  }
+  constructor() {
+    this.items = [];
+  }
+  addNote(note) {
+    this.items.push(note);
+  }
+  removeNote(text) {
+    const indexElem = this.items.findIndex((element) => element.text === text);
+    this.items.splice(indexElem, 1);
+  }
+  updatePriority(text, newPriority) {
+    const indexElem = this.items.findIndex((element) => element.text === text);
+    this.items[indexElem].priority = newPriority;
+  };
+}
+
+const note1 = new Notes();
+note1.addNote({ text: "Note1", priority: Notes.Priority().LOW });
+note1.addNote({ text: "Note2", priority: Notes.Priority().LOW });
+note1.addNote({ text: "Note3", priority: Notes.Priority().LOW });
+note1.removeNote('Note2');
+note1.updatePriority("Note1", Notes.Priority().HIGHT);
+console.log(note1);
